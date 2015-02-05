e7af63fbfeff2758bf0cc95ac8d4bd600ebab0f3
LucasRenato <lucas.mas@fatec.sp.gov.br>
2015-02-05 15:07:49 -0200

    NumberWizardUI
    Third Game Course

---
+++ Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class NumberWizards : MonoBehaviour {
+	// Use this for initialization
+	public Text guessText;
+	
+	int max = 10000;
+	int min = 1;
+	int guess;
+	
+	
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {
+		max = max + 1;
+		NextGuess();
+	}
+	
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+	
+	void NextGuess () {		
+		guess = Random.Range(min, max);
+		guessText.text = guess.ToString();
+	}
+}
